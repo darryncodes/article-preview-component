e5e574b0b03db092d61d70cd97734189388ff953
Darryn Codes <hello@darryncodes.co.uk>
2021-08-27 20:47:10 +0100

    adding all styles and functionality

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function toggle(social) {
+  let popup = document.getElementById('social');
+  if (popup.style.display == 'flex') popup.style.display = 'none';
+  else popup.style.display = 'flex';
+}
